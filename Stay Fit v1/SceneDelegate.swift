6b6d4ec5fca6e1d23355251ffa34ec5ef589a024
Vlad Balas <balash_vlad@yahoo.com>
2021-07-30 00:04:47 +0100

    Navigation Bar back button CHANGED to black

--- Stay Fit v1/SceneDelegate.swift
+++ Stay Fit v1/SceneDelegate.swift
@@ -13,6 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     //@StateObject var exerciseViewModel: ExerciseViewModel = ExerciseViewModel()
+    
+    
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -33,6 +35,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
+            window.tintColor = .black  //  !!!!!!!!!! CHANGES THE NAVIGATION BAR BACK BUTTON TO COLOR BLACK !!!!!!!!!!
             self.window = window
             window.makeKeyAndVisible()
         }
